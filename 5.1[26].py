82ed21d00e67b08ba81b576d2d2ad75342caa4c5
Nataliia <nataliyatrishechkina@mail.ru>
2023-02-13 16:40:26 +0300

    added code for 5.1[26]

--- 5.1[26].py
+++ 5.1[26].py
@@ -6,3 +6,16 @@
 # <function_name>(2,1) -> 2
 # <function_name>(2,3) -> 8
 # <function_name>(2,4) -> 16
+
+def power_number (a, b):
+    if b == 0:
+        return 1
+    elif b == 1:
+        return a
+    else:
+        return a * power_number(a, b - 1)
+        
+
+a = int(input('a = '))
+b = int(input('b = '))
+print(power_number (a, b))   
